c3baad4b081ccfddf34b1fc3721ececd6c8ce7f3
DontitoSama <jokonito06@gmail.com>
2024-05-25 15:57:38 +0200

    :pencil: Adding fonts, and loading screen

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as Font from 'expo-font';
+import HomeScreen from './screens/HomeScreen';
+import SplashScreen from './screens/SplashScreen';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        'SF-Ultralight': require('./assets/fonts/SF-Pro-Text-Ultralight.otf'),
+        'SF-Regular': require('./assets/fonts/SF-Pro-Text-Regular.otf'),
+        'SF-RegularItalic': require('./assets/fonts/SF-Pro-Text-RegularItalic.otf'),
+        'SF-Semibold': require('./assets/fonts/SF-Pro-Text-Semibold.otf'),
+        'SF-SemiboldItalic': require('./assets/fonts/SF-Pro-Text-SemiboldItalic.otf'),
+        'SF-Text-Thin': require('./assets/fonts/SF-Pro-Text-Thin.otf'),
+        'SF-ThinItalic': require('./assets/fonts/SF-Pro-Text-ThinItalic.otf'),
+        'SF-Ultralight': require('./assets/fonts/SF-Pro-Text-Ultralight.otf'),
+        'SF-UltralightItalic': require('./assets/fonts/SF-Pro-Text-UltralightItalic.otf'),
+      });
+      setFontsLoaded(true);
+    };
+
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Loading Fonts...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+
+
---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,13 @@
+// screens/HomeScreen.js
+import React from 'react';
+import { View } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {/* Votre contenu pour l'écran d'accueil */}
+    </View>
+  );
+};
+
+export default HomeScreen;
