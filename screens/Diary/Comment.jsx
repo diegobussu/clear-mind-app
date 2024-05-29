b9084fd2a3222db33a178f9327dfd327d6382a39
DontitoSama <jokonito06@gmail.com>
2024-05-29 16:10:04 +0200

    :pencil: Workind until Comment

--- screens/Diary/Comment.jsx
+++ screens/Diary/Comment.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, Alert, TouchableOpacity, PanResponder } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, TextInput, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Button from '../../components/Button';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -25,21 +25,26 @@ const Comment = ({ route }) => {
   const auth = getAuth(app);
   const db = getFirestore(app);
   const userId = auth.currentUser?.uid;
+  const [note, setNote] = useState('');
 
   const continueHandler = async () => {
+    try {
 
-    const journalID = route.params.journalID;
-    const journalRef = doc(db, 'users', userId, 'journals', journalID);
-    await updateDoc(journalRef, {
-      emotions: emotions,
-      updatedAt: Timestamp.now()
-    });
+      const journalID = route.params.journalID;
+      const journalRef = doc(db, 'users', userId, 'journals', journalID);
+      await updateDoc(journalRef, {
+        note: note,
+        updatedAt: Timestamp.now()
+      });
+    } catch (error) {
+      Alert.alert('Erreur', 'Une erreur s\'est produite lors de l\'ajout de la note.');
+    }
   };
 
 
   return (
     <SafeAreaView className="flex-1 justify-center items-center text-center px-5 bg-secondary-white">
-      <View className="flex-row mt-5">
+      <View className="flex-row">
           <TouchableOpacity onPress={() => navigation.goBack()}>
               <Ionicons
                   name="arrow-back-circle"
@@ -56,10 +61,19 @@ const Comment = ({ route }) => {
               />
           </TouchableOpacity>
       </View>
-      <View style={styles.rectangle} className="mt-5">
+      <View style={styles.rectangle} className="mt-[100px]">
         <Image source={selectedImage} style={styles.image} resizeMode="contain" />
         <Text className="font-sf-regular text-xl mt-3">Aujourd'hui, je me sens</Text>
         <Text className="font-sf-bold text-xl mt-3">{mood}</Text>
+      </View>
+      <Text className="font-sf-regular text-2xl mt-10">Note</Text>
+      <View className="w-[350px] h-[200px] bg-primary-white rounded-[30px] mt-5 mb-10">
+        <TextInput
+          className="text-lg text-[#828282] top-5 left-5"
+          placeholder="Ajouter une note..."
+          onChangeText={setNote}
+          value={note}
+        />
       </View>
       <Button text="Terminer" onPress={continueHandler} />
     </SafeAreaView>
