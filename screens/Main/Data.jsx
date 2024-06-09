082eef30f991926093c1a7485f518ad1eaf7e4b8
DontitoSama <jokonito06@gmail.com>
2024-06-09 15:16:20 +0200

    :pencil: Refresh data

--- screens/Main/Data.jsx
+++ screens/Main/Data.jsx
@@ -4,32 +4,8 @@ import { getAuth } from "firebase/auth";
 import { doc, getDoc, getFirestore, setDoc, Timestamp, collection, query, addDoc, getDocs, updateDoc } from "firebase/firestore";
 import { app } from "../../firebaseConfig";
 import moment from 'moment';
-import { Calendar, LocaleConfig } from 'react-native-calendars';
 
-LocaleConfig.locales['fr'] = {
-  monthNames: [
-    'Janvier',
-    'Février',
-    'Mars',
-    'Avril',
-    'Mai',
-    'Juin',
-    'Juillet',
-    'Août',
-    'Septembre',
-    'Octobre',
-    'Novembre',
-    'Décembre'
-  ],
-  monthNamesShort: ['Janv.', 'Févr.', 'Mars', 'Avril', 'Mai', 'Juin', 'Juil.', 'Août', 'Sept.', 'Oct.', 'Nov.', 'Déc.'],
-  dayNames: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
-  dayNamesShort: ['Dim.', 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.'],
-  today: "Aujourd'hui"
-};
-
-LocaleConfig.defaultLocale = 'fr';
-
-const Data = () => {
+const Data = ({ currentMonth, currentYear }) => {
   const auth = getAuth(app);
   const db = getFirestore(app);
   const userId = auth.currentUser?.uid;
@@ -37,7 +13,7 @@ const Data = () => {
   const [totalEntries, setTotalEntries] = useState(0);
   const [consecutiveEntries, setConsecutiveEntries] = useState(0);
   const [longestStreak, setLongestStreak] = useState(0);
-  const [markedDates, setMarkedDates] = useState({});
+  const [connectionDays, setConnectionDays] = useState([]);
 
   useEffect(() => {
     const fetchUserEntries = async () => {
@@ -48,8 +24,46 @@ const Data = () => {
           const entriesCollectionRef = collection(userDocRef, 'entries');
           const entriesQuery = query(entriesCollectionRef);
           const entriesSnapshot = await getDocs(entriesQuery);
-          const entriesData = entriesSnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+          const uniqueDates = new Set();
+          const entriesData = entriesSnapshot.docs.map(doc => {
+            const formattedDate = moment(doc.data().date.toDate()).format('YYYY-MM-DD');
+            uniqueDates.add(formattedDate);
+            return { ...doc.data(), id: doc.id, date: formattedDate };
+          });
           setUserEntries(entriesData);
+
+          const connectionDays = [...uniqueDates].filter(date => {
+            const [year, month] = date.split('-');
+            return Number(year) === currentYear && Number(month) === currentMonth;
+          });
+          setConnectionDays(connectionDays);
+
+          // Calculer les entrées consécutives, totales et la plus longue série
+          let currentConsecutiveEntries = 0;
+          let currentTotalEntries = 0;
+          let longestStreakCount = 0;
+          let currentStreakCount = 0;
+          let prevDate = null;
+
+          for (const entry of entriesData) {
+            const entryDate = moment(entry.date);
+            if (entryDate.year() === currentYear && entryDate.month() + 1 === currentMonth) {
+              currentTotalEntries += entry.count;
+              if (!prevDate || entryDate.diff(prevDate, 'days') === 1) {
+                currentConsecutiveEntries += entry.count;
+                currentStreakCount += entry.count;
+              } else if (entryDate.diff(prevDate, 'days') > 1) {
+                currentConsecutiveEntries = entry.count;
+                currentStreakCount = entry.count;
+              }
+              longestStreakCount = Math.max(longestStreakCount, currentStreakCount);
+              prevDate = entryDate;
+            }
+          }
+
+          setTotalEntries(currentTotalEntries);
+          setConsecutiveEntries(currentConsecutiveEntries);
+          setLongestStreak(longestStreakCount);
         } else {
           await setDoc(userDocRef, {});
           setUserEntries([]);
@@ -60,7 +74,8 @@ const Data = () => {
     };
 
     fetchUserEntries();
-  }, [userId]);
+  }, [userId, currentMonth, currentYear]);
+
 
   useEffect(() => {
     const total = userEntries.reduce((acc, entry) => acc + entry.count, 0);
@@ -69,13 +84,6 @@ const Data = () => {
     let longestStreakCount = 0;
     let currentStreakCount = 0;
     let prevDate = null;
-
-    const formattedDates = userEntries.map(entry => moment.utc(entry.date).utcOffset('+0200').format('YYYY-MM-DD'));
-    const markedDatesObj = formattedDates.reduce((acc, date) => {
-      acc[date] = { selected: true, selectedColor: '#6331FF' };
-      return acc;
-    }, {});
-    setMarkedDates(markedDatesObj);
 
     userEntries.sort((a, b) => moment(a.date).diff(moment(b.date)));
 
@@ -95,7 +103,7 @@ const Data = () => {
   }, [userEntries]);
 
   const incrementEntryCount = async () => {
-    const today = moment().utcOffset('+0200').startOf('day');
+    const today = moment().startOf('day');
     const todayEntry = userEntries.find(entry => moment(entry.date).startOf('day').isSame(today, 'day'));
 
     if (todayEntry) {
@@ -125,15 +133,12 @@ const Data = () => {
         <View className="rounded-xl p-5 mx-5 bg-primary-white">
           <Text className="font-Qs-Bold text-xl mb-5">Entrée consécutive : {consecutiveEntries}</Text>
 
-          <Calendar
-            locale="fr"
-            theme={{
-              arrowColor: '#6331FF'
-            }}
-            firstDay={1}
-            style={{ backgroundColor: '#FFF', width: 300 }}
-            markedDates={markedDates}
-          />
+          <Text className="font-Qs-SemiBold text-[18px] mt-3">Jours de connexion :</Text>
+          <View>
+            {connectionDays.map((day, index) => (
+              <Text key={index}>{moment(day).format('dddd DD')}</Text>
+            ))}
+          </View>
 
           <Text className="font-Qs-SemiBold text-[18px] mt-10">Entrées totales : {totalEntries}</Text>
           <Text className="font-Qs-SemiBold text-[18px] mt-3">Plus longue série : {longestStreak}</Text>
