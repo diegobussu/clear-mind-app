2ea2977c332226fea717cff027af11226931c64a
DontitoSama <jokonito06@gmail.com>
2024-06-24 10:35:58 +0200

    :pencil: Add unitTest

---
+++ __tests__/components/ButtonWhite.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import Button from '../../components/ButtonWhite';
+
+describe('Button Component', () => {
+  it('rend du texte', () => {
+    const { getByText } = render(<Button text="Test Button" onPress={() => {}} />);
+    expect(getByText('Test Button')).toBeTruthy();
+  });
+
+  it('onPress fonctionnel', () => {
+    const onPressMock = jest.fn();
+    const { getByText } = render(<Button text="Test Button" onPress={onPressMock} />);
+
+    fireEvent.press(getByText('Test Button'));
+
+    expect(onPressMock).toHaveBeenCalledTimes(1);
+  });
+});
